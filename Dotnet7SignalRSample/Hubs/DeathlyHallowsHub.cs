dbf43d96f58915d203077515b0f453c0acc2d408
muratilgun <muratilgun34@gmail.com>
2022-06-14 21:05:34 +0300

    Race

---
+++ Dotnet7SignalRSample/Hubs/DeathlyHallowsHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Dotnet7SignalRSample.Hubs;
+
+public class DeathlyHallowsHub : Hub
+{
+    public Dictionary<string, int> GetRaceStatus()
+    {
+        return SD.DealthyHallowRace;
+    }
+}
