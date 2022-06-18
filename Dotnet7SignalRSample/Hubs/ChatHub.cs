50b64fa522ca417fba55e48d0ad63a6251bcc1d4
muratilgun <muratilgun34@gmail.com>
2022-06-18 22:28:23 +0300

    Send Public Message

---
+++ Dotnet7SignalRSample/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Dotnet7SignalRSample.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessageToAll(string user, string message)
+    {
+        await Clients.All.SendAsync("MessageReceived", user,message);
+    }
+}
