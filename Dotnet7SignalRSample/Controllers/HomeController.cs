44144c1dc6ec31a16b842f5e1c7281aab6ebee91
muratilgun <muratilgun34@gmail.com>
2022-06-19 19:29:10 +0300

    Create Order Methods

--- Dotnet7SignalRSample/Controllers/HomeController.cs
+++ Dotnet7SignalRSample/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Dotnet7SignalRSample.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Dotnet7SignalRSample.Data;
 using Dotnet7SignalRSample.Hubs;
 using Microsoft.AspNetCore.SignalR;
 
@@ -10,13 +11,14 @@ namespace Dotnet7SignalRSample.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IHubContext<DeathlyHallowsHub> _deathlyHub;
+        private readonly ApplicationDbContext _context;
 
 
-
-        public HomeController(ILogger<HomeController> logger, IHubContext<DeathlyHallowsHub> deathlyHub)
+        public HomeController(ILogger<HomeController> logger, IHubContext<DeathlyHallowsHub> deathlyHub, ApplicationDbContext context)
         {
             _logger = logger;
             _deathlyHub = deathlyHub;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -40,28 +42,63 @@ namespace Dotnet7SignalRSample.Controllers
 
             return Accepted();
         }
-
         public IActionResult Notification()
         {
             return View();
         }
-
         public IActionResult DeathlyHallowRace()
         {
             return View();
         }
-
-
         public IActionResult HarryPotterHouse()
         {
             return View();
         }
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [ActionName("Order")]
+        public async Task<IActionResult> Order()
+        {
+            string[] name = { "Bhrugen", "Ben", "Jess", "Laura", "Ron" };
+            string[] itemName = { "Food1", "Food2", "Food3", "Food4", "Food5" };
+
+            Random rand = new Random();
+            // Generate a random index less than the size of the array.  
+            int index = rand.Next(name.Length);
+
+            Order order = new Order()
+            {
+                Name = name[index],
+                ItemName = itemName[index],
+                Count = index
+            };
+
+            return View(order);
+        }
+
+        [ActionName("Order")]
+        [HttpPost]
+        public async Task<IActionResult> OrderPost(Order order)
+        {
+
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Order));
+        }
+        [ActionName("OrderList")]
+        public async Task<IActionResult> OrderList()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetAllOrder()
+        {
+            var productList = _context.Orders.ToList();
+            return Json(new { data = productList });
         }
     }
 }
